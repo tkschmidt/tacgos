7e977573a440a81c26cf77af4bd2d7f841add7a3
Tobias <tobias@tkschmidt.me>
2015-12-29 15:41:20 +0100

    start testing v3

--- player/player_test.go
+++ player/player_test.go
@@ -2,17 +2,15 @@ package player
 
 import (
 	tl "github.com/JoelOtter/termloop"
-	"github.com/tkschmidt/tacgos/cursor"
 	"github.com/tkschmidt/tacgos/level"
-	"github.com/tkschmidt/tacgos/player"
 	"testing"
 )
 
 func TestVision(t *testing.T) {
 	game := tl.NewGame()
 	level := level.GenerateLvl()
-	pl := player.GeneratePlayer(level, game)
-	if 1 != 1 {
-		t.Error("Expected True")
+	pl := GeneratePlayer(level, game)
+	if pl.direction != 1 {
+		t.Error("Expected 1", pl.direction)
 	}
 }
